17fe6dc769a121fd18b98c065c58334dcf99da27
Marijn <42944527+ThatGuyMarijn@users.noreply.github.com>
2020-05-29 12:43:15 +0200

    Initial Commit

---
+++ Modules/LeerlingMenu.php
@@ -0,0 +1,20 @@
+<?php
+    if(LoginCheck($pdo))
+    {
+        if($_SESSION["role"] == "Student")
+        {
+            
+        }
+        else
+        {
+            echo "Je hebt niet de juiste bevoegdheid voor deze pagina.";
+            // TODO: redirect de persoon terug naar zijn rol behorende home pagina
+            RedirectToPage(3, NULL);
+        }
+    }
+    else
+    {
+        // Redirect naar login pagina
+        RedirectToPage();
+    }
+?>
