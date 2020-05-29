47e1f3e49fa98bc341d58446b352c5aa4d29c062
Marijn <42944527+ThatGuyMarijn@users.noreply.github.com>
2020-05-29 14:10:25 +0200

    Update RegistrerenForm.php

--- Forms/RegistrerenForm.php
+++ Forms/RegistrerenForm.php
@@ -11,9 +11,17 @@
     <label>Bevestig Wachtwoord: </label>
     <input type="password" name="retypePassword" /><?php echo $rePasswordErr; ?>
     <br />
+    <label>Leraar of Student?</label>
     <select name="role">
-        <option value="teacher">Leraar</option>
-        <option value="student">Student</option>
+        <option value="Teacher">Leraar</option>
+        <option value="Student">Student</option>
+    </select>
+    <br />
+    <label>In welke groep zit hij/zij?</label>
+    <select name="class">
+        <option value="4">Groep 4</option>
+        <option value="5">Groep 5</option>
+        <option value="6">Groep 6</option>
     </select>
     <br />
     <input type="submit" name="registreren" value="Registreren" />
