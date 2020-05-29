c09b87170e1deaca7aa8429f166f6a88303890d1
Marijn <42944527+ThatGuyMarijn@users.noreply.github.com>
2020-05-29 10:20:28 +0200

    Added $salt optional parameter to SecurePassword()

---
+++ Functies.php
@@ -0,0 +1,48 @@
+<?php
+    function ConnectDB()
+    {
+        $pdo = new PDO("mysql:host=localhost;dbname=desplinter", "root");
+        return $pdo;
+    }
+
+    function RedirectToPage($seconds = NULL, $paginaNr = NULL)
+    {
+        if(!empty($seconds))
+            $refresh = "Refresh: ".$seconds.";URL=";
+        else
+            $refresh = "location:";
+
+        if(!isset($pginaNr))
+        {
+            echo "<br />Je wordt binnen ".$seconds." seconden doorgestuurd naar de hoofdpagina.";
+            header($refresh . "index.php");
+        }
+        else
+        {
+            header($refresh . "index.php?paginaNr=".$paginaNr);
+        }
+    }
+
+    function LoginCheck($pdo)
+    {
+        // TODO: LoginCheck moet nog gemaakt worden, overnemen van cinema7?
+        return true;
+    }
+
+    function SecurePassword($password = NULL, $salt = NULL)
+    {
+        // hier maken we nog een salt aan
+        if(isset($password))
+        {
+            $password = hash("sha512", $password . $salt);
+            echo "<script>console.log('hashed: $password');</script>";
+            return $password;
+        }
+        else
+        {
+            // voor als de salt nodig hebben voor in de database
+            return $salt;
+        }
+        
+    }
+?>
