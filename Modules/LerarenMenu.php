975fc6a573be5aebeabcb2d56ff522c4f296e6af
Marijn <42944527+ThatGuyMarijn@users.noreply.github.com>
2020-06-03 09:34:07 +0200

    Update LerarenMenu.php

--- Modules/LerarenMenu.php
+++ Modules/LerarenMenu.php
@@ -1,3 +1,24 @@
 <?php
-
+    if(LoginCheck($pdo))
+    {
+        if($_SESSION["role"] == "Teacher")
+        {
+            // Hier komt de leraren pagina
+        }
+        elseif($_SESSION["role"] == "Student")
+        {
+            // Redirect naar leerlingen pagina
+            RedirectToPage(0, 10);
+        }
+        else
+        {
+            // Redirect naar login pagina
+            RedirectToPage();
+        }
+    }
+    else
+    {
+        // Redirect naar login pagina
+        RedirectToPage();
+    }
 ?>
