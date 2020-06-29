d7cbf7cd188d00b1f1fc04720c6071bffcf65e0e
Ponyprinses <55741812+Ponyprinses@users.noreply.github.com>
2020-06-29 11:14:02 +0200

    regga

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" media= "screen" href="css/style.css" >
   <title>Rekenwebsite - De Splinter</title>
   <?php
 		session_start();
